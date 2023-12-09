94b1e8865afa6c83c98b93cd0ec1b45af59bf6e0
Takuya-ops <hora2112.ipu@gmail.com>
2023-12-09 16:52:59 +0900

    日本語名が入るように修正しました。

--- seeder.py
+++ seeder.py
@@ -21,22 +21,38 @@ def fetch_pokemon_data(pokemon_id):
     api_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
     try:
         response = requests.get(api_url)
-        response.raise_for_status()  # ステータスコードが200系以外の場合、例外を発生させる
-        return response.json()
-    except requests.exceptions.HTTPError as errh:
-        print(f"Http Error: {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        print(f"Error Connecting: {errc}")
-    except requests.exceptions.Timeout as errt:
-        print(f"Timeout Error: {errt}")
-    except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
-    return None
+        response.raise_for_status()
+        data = response.json()
+
+        # ポケモンの種族情報を取得するためのURL
+        species_url = data["species"]["url"]
+
+        # 種族情報から日本語の名前を取得
+        species_response = requests.get(species_url)
+        species_response.raise_for_status()
+        species_data = species_response.json()
+
+        # 日本語の名前を探す
+        for name in species_data["names"]:
+            if name["language"]["name"] == "ja":
+                japanese_name = name["name"]
+                break
+        else:
+            japanese_name = None  # 日本語の名前が見つからない場合
+
+        return {
+            "name_jp": japanese_name,
+            "height": data["height"],
+            "weight": data["weight"],
+        }
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
 
 
 # データベースにポケモンのデータを保存
 def save_pokemon_to_db(pokemon_data):
-    if pokemon_data is None:
+    if pokemon_data is None or pokemon_data["name_jp"] is None:
         return
 
     try:
@@ -44,29 +60,27 @@ def save_pokemon_to_db(pokemon_data):
             dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
         ) as conn:
             with conn.cursor() as cursor:
-                # テーブルが存在しない場合、作成
                 cursor.execute(
                     """
                     CREATE TABLE IF NOT EXISTS pokemon (
                         id SERIAL PRIMARY KEY,
-                        name VARCHAR(255),
+                        name_jp VARCHAR(255),
                         height FLOAT,
                         weight FLOAT
                     )
                     """
                 )
 
-                # データを挿入
                 cursor.execute(
-                    "INSERT INTO pokemon (name, height, weight) VALUES (%s, %s, %s)",
+                    "INSERT INTO pokemon (name_jp, height, weight) VALUES (%s, %s, %s)",
                     (
-                        pokemon_data["name"],
+                        pokemon_data["name_jp"],
                         pokemon_data["height"],
                         pokemon_data["weight"],
                     ),
                 )
                 conn.commit()
-                print(f"Saved Pokemon {pokemon_data['name']} to database.")
+                print(f"Saved Pokemon {pokemon_data['name_jp']} to database.")
     except psycopg2.DatabaseError as e:
         print(f"Database Error: {e}")
     except Exception as e:
