a881b755a08063d6cea951fb9393f1615c9a0240
Takuya-ops <hora2112.ipu@gmail.com>
2023-12-09 18:10:29 +0900

    typeも日本語になるように修正

--- seeder.py
+++ seeder.py
@@ -3,6 +3,7 @@ import requests
 import psycopg2
 import sys
 import os
+import json
 from dotenv import load_dotenv
 
 # .envファイルの読み込み
@@ -21,7 +22,7 @@ def fetch_pokemon_data(pokemon_id):
     api_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
     try:
         response = requests.get(api_url)
-        response.raise_for_status()
+        response.raise_for_status()  # ステータスコードが200系以外の場合、例外を発生させる
         data = response.json()
 
         # ポケモンの種族情報を取得するためのURL
@@ -32,22 +33,52 @@ def fetch_pokemon_data(pokemon_id):
         species_response.raise_for_status()
         species_data = species_response.json()
 
-        # 日本語の名前を探す
+        japanese_name = None
         for name in species_data["names"]:
             if name["language"]["name"] == "ja":
                 japanese_name = name["name"]
                 break
-        else:
-            japanese_name = None  # 日本語の名前が見つからない場合
+
+        # タイプの日本語名を取得
+        types = []
+        for type_info in data["types"]:
+            type_url = type_info["type"]["url"]
+            type_response = requests.get(type_url)
+            type_response.raise_for_status()
+            type_data = type_response.json()
+
+            japanese_type_name = None
+            for name in type_data["names"]:
+                if name["language"]["name"] == "ja":
+                    japanese_type_name = name["name"]
+                    break
+
+            if japanese_type_name:
+                types.append(japanese_type_name)
+
+        # 統計データを取得
+        stats = {s["stat"]["name"]: s["base_stat"] for s in data["stats"]}
+
+        # 画像のURLを取得
+        image_url = data["sprites"]["front_default"]
 
         return {
             "name_jp": japanese_name,
             "height": data["height"],
             "weight": data["weight"],
+            "types": types,
+            "stats": stats,
+            "image_url": image_url,
         }
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+    return None
 
 
 # データベースにポケモンのデータを保存
@@ -60,23 +91,34 @@ def save_pokemon_to_db(pokemon_data):
             dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
         ) as conn:
             with conn.cursor() as cursor:
+                # テーブルが存在しない場合、作成
                 cursor.execute(
                     """
                     CREATE TABLE IF NOT EXISTS pokemon (
                         id SERIAL PRIMARY KEY,
                         name_jp VARCHAR(255),
                         height FLOAT,
-                        weight FLOAT
+                        weight FLOAT,
+                        types VARCHAR(255),
+                        stats JSONB,
+                        image_url TEXT
                     )
                     """
                 )
 
+                # JSON形式で統計データを保存
+                stats_json = json.dumps(pokemon_data["stats"])
+
+                # データを挿入
                 cursor.execute(
-                    "INSERT INTO pokemon (name_jp, height, weight) VALUES (%s, %s, %s)",
+                    "INSERT INTO pokemon (name_jp, height, weight, types, stats, image_url) VALUES (%s, %s, %s, %s, %s, %s)",
                     (
                         pokemon_data["name_jp"],
                         pokemon_data["height"],
                         pokemon_data["weight"],
+                        ",".join(pokemon_data["types"]),
+                        stats_json,
+                        pokemon_data["image_url"],
                     ),
                 )
                 conn.commit()
@@ -88,7 +130,7 @@ def save_pokemon_to_db(pokemon_data):
 
 
 def main():
-    for pokemon_id in range(1, 11):  # 例として最初の10匹のポケモンを取得
+    for pokemon_id in range(1, 152):  # 例として最初の151匹のポケモンを取得
         pokemon_data = fetch_pokemon_data(pokemon_id)
         save_pokemon_to_db(pokemon_data)
 
